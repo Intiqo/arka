3a3b9f4afea017336c83422092bd399ee2b8ea33
Vishwas Shashidhar <vishwas@adwitiya.io>
2023-08-28 14:46:57 +0530

    load aws credentials from profile

--- cloud/aws_manager.go
+++ cloud/aws_manager.go
@@ -5,11 +5,9 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsCfg "github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
 
 	"github.com/adwitiyaio/arka/config"
 	"github.com/adwitiyaio/arka/dependency"
-	"github.com/adwitiyaio/arka/logger"
 )
 
 const regionKey = "AWS_REGION"
@@ -38,16 +36,13 @@ func (am *awsManager) initialize() {
 	var err error
 	am.config, err = awsCfg.LoadDefaultConfig(context.TODO())
 	if err != nil {
-		logger.Log.Error().Err(err).Msg("failed to load default aws config. Trying with environemt variables")
-		am.region = cm.GetValueForKey(regionKey)
-		accessKeyId := cm.GetValueForKey(accessKeyIdKey)
-		secretAccessKey := cm.GetValueForKey(secretAccessKey)
-		if am.region == "" || accessKeyId == "" || secretAccessKey == "" {
-			logger.Log.Fatal().Msg("Missing AWS credentials. Please set AWS_REGION, AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables")
+		awsProfile := cm.GetValueForKey("AWS_PROFILE")
+		if awsProfile == "" {
+			awsProfile = "default"
 		}
-		am.config = *&aws.Config{
-			Region:      am.region,
-			Credentials: credentials.NewStaticCredentialsProvider(accessKeyId, secretAccessKey, ""),
+		am.config, err = awsCfg.LoadDefaultConfig(context.TODO(), awsCfg.WithSharedConfigProfile(awsProfile))
+		if err != nil {
+			panic(err)
 		}
 	}
 }
